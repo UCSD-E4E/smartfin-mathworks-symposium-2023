685ae60ed8660227a14f85a7724da47cead83271
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2023-05-18 01:41:19 -0700

    add additional comments to function

--- filter_derived_heave.m
+++ filter_derived_heave.m
@@ -4,6 +4,8 @@ function [acceleration, velocity, position] = filter_derived_heave(sensor_data, 
             error("sensor_data is not a table.");
         end
     
+        % We do not check for Magnetometer here since we will use that to
+        % determine if we use ahrsfilter or imufilter.
         valid = any("Accelerometer" == string(sensor_data.Properties.VariableNames));
         valid = valid && any("Gyroscope" == string(sensor_data.Properties.VariableNames));
     
@@ -13,7 +15,9 @@ function [acceleration, velocity, position] = filter_derived_heave(sensor_data, 
     end
 
     function rotation = get_world_rotation(sensor_data, sampling_rate)
+        % If we have the magnetometer data, let's use it in the filter.
         use_ahrs = any("Magnetometer" == string(sensor_data.Properties.VariableNames));
+        
         if use_ahrs
             fuse = ahrsfilter('SampleRate', sampling_rate, 'ReferenceFrame', 'ENU', 'OrientationFormat', 'Rotation matrix');
             rotation = fuse(sensor_data.Accelerometer, sensor_data.Gyroscope, sensor_data.Magnetometer);
@@ -24,6 +28,8 @@ function [acceleration, velocity, position] = filter_derived_heave(sensor_data, 
     end
 
     function world_acceleration = get_world_acceleration(accelerometer, world_rotation)
+        % Use the world rotation matrix to switch from body frame to world
+        % frame.
         world_acceleration = zeros(size(sensor_data.Accelerometer));
         for idx = 1:size(world_acceleration, 1)
             world_acceleration(idx, :) = world_rotation(:, :, idx)' * accelerometer(idx, :)';
@@ -31,12 +37,15 @@ function [acceleration, velocity, position] = filter_derived_heave(sensor_data, 
     end
 
     function [velocity, position] = get_velocity_position(world_acceleration_z, sampling_frequency)
+        % Remove the gravity acceleration
         world_acceleration_z = world_acceleration_z + 9.80665;
 
+        % Create vectors for us to store the final results in.
         count = size(world_acceleration_z, 1);
         velocity = zeros(count, 1);
         position = zeros(count, 1);
 
+        % Double intergrate
         for idx = 1:count
             velocity(idx) = sum(world_acceleration_z(1:idx)) * (1 / sampling_frequency);
             position(idx) = sum(velocity(1:idx)) * (1 / sampling_frequency);
@@ -46,6 +55,7 @@ function [acceleration, velocity, position] = filter_derived_heave(sensor_data, 
     validate_sensor_data(sensor_data);
     world_rotation = get_world_rotation(sensor_data, sampling_rate);
     acceleration = get_world_acceleration(sensor_data.Accelerometer, world_rotation);
+    % We down sample to reduce error from double integration.
     world_acceleration_z = downsample(acceleration(:, 3), int32(sampling_rate) / target_frequency);
 
     [velocity, position] = get_velocity_position(world_acceleration_z, target_frequency);
